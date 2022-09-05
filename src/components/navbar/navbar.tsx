b21e4c710851e139109c9ca5ebf71a482e1c53dc
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2022-09-05 14:39:42 +0100

    Created Navbar

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './navbar.css';
+
+const Navbar = () => {
+    return (
+        <div className="navBar">
+            <nav>
+                <ul>
+                    <Link className="navBar__link" to="/">
+                        Home
+                    </Link>
+
+                    <Link className="navBar__link" to="/apod">
+                        APOD
+                    </Link>
+
+                    <Link className="navBar__link" to="/jwst">
+                        JWST
+                    </Link>
+                    <Link className="navBar__link" to="/launch">
+                        Launches
+                    </Link>
+                </ul>
+
+                {/* <Link to="/apod">APOD</Link>
+
+            <Link to="/jwst">JWST</Link>
+
+            <Link to="/launches">LAUNCHES</Link> */}
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
