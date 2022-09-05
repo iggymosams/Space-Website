93e35179344855aac315783d552a8be962515357
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2022-09-05 14:38:53 +0100

    Refactored CSS

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,34 @@
+import React, { useCallback } from 'react';
+import { Link } from 'react-router-dom';
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
+import { Container, Engine } from 'tsparticles-engine';
+import Stars from '../../components/stars/stars';
+import './home.css';
+
+const Home = () => {
+    return (
+        <div
+            className="home noselect"
+            onContextMenu={(e) => e.preventDefault()}
+        >
+            <div className="home__container">
+                <h1 className="home__title">IGGYMOSAMS'S SPACE WEBSITE</h1>
+                <div className="homeNav">
+                    <Link className="homeNav__link" to="/apod">
+                        APOD
+                    </Link>
+                    <Link className="homeNav__link" to="/jwst">
+                        JWST
+                    </Link>
+                    <Link className="homeNav__link" to="/launches">
+                        Launches
+                    </Link>
+                </div>
+            </div>
+            <Stars />
+        </div>
+    );
+};
+
+export default Home;
