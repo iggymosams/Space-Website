e742abc0d8eb7d9f9e99dcaf856f428bea2f7d4d
IGGYMOSAMS <78950419+iggymosams@users.noreply.github.com>
2022-09-07 02:12:19 +0100

    Added Launches Page and Added Query Data to APOD

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
+import { queryAllByAltText } from '@testing-library/react';
 import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import {
+    Route,
+    BrowserRouter as Router,
+    Routes,
+    useLocation,
+} from 'react-router-dom';
 import APOD from './pages/APOD/APOD';
 import Home from './pages/home/Home';
+import Launches from './pages/launches/launches';
 import NoPage from './pages/NoPage';
 
+function useQuery() {
+    const { search } = useLocation();
+
+    return React.useMemo(() => new URLSearchParams(search), [search]);
+}
+
 function App() {
+    let query = useQuery();
     return (
-        <Router>
-            <Routes>
-                <Route index element={<Home />} />
-                <Route path="/apod" element={<APOD />} />
-                <Route path="*" element={<NoPage />} />
-            </Routes>
-        </Router>
+        <Routes>
+            <Route index element={<Home />} />
+            <Route
+                path="/apod"
+                element={<APOD queryDate={query.get('date')} />}
+            />
+            <Route path="/launches" element={<Launches />} />
+            <Route path="*" element={<NoPage />} />
+        </Routes>
     );
 }
 
